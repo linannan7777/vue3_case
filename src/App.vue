bccb42da72a73eaeef2a981e59291ae085162cf7
linannan <linannan@prosnav.com>
2024-04-18 16:03:59 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div class="box">
+      <el-button type="primary">Primary</el-button>
+      <el-button :icon="Search" circle />
+      <el-button type="primary" :icon="Edit" circle />
+      <el-button type="success" :icon="Check" circle />
+      <el-button type="info" :icon="Message" circle />
+      <el-button type="warning" :icon="Star" circle />
+      <el-button type="danger" :icon="Delete" circle />
+    </div>
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+    <h1>测试svg</h1>
+    <svg>
+      <use xlink:href="#icon-logo" fill="red"></use>
+    </svg>
+    <SvgIcon name="logo"></SvgIcon>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
+const currentPage4 = ref(4)
+const pageSize4 = ref(4)
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`)
+}
+console.log(import.meta.env)
+</script>
+<style scoped lang="scss">
+  .box {
+    background-color: $color;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// main.ts
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import App from './App.vue'
+import 'virtual:svg-icons-register'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+import gloablComponent from './components/index';
+app.use(gloablComponent);
+app.mount('#app')
